12d132b7da7e0457cdf6cb019b0a86823cfe0ffa
Hattori-Arch <hattori.index.js@asagi.waseda.jp>
2026-04-28 16:44:01 +0900

    KVのセットアップ

--- apps/api/src/api.ts
+++ apps/api/src/api.ts
@@ -1,3 +1,13 @@
 import { Hono } from "hono"
 
-export const api = new Hono().get("/", (c) => c.json({ msg: "API" }))
+export const api = new Hono<{ Bindings: Env }>()
+  .get("/", (c) => c.json({ msg: "API" }))
+  .get("/kv", async (c) => {
+    const data = await c.env.BOX_REFRESH_TOKEN_CACHE.get("test")
+    if (data) {
+      return c.text(`loaded data: ${data}`)
+    }
+
+    c.env.BOX_REFRESH_TOKEN_CACHE.put("test", "Test Data")
+    return c.text("put data")
+  })
