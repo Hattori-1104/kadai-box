a63764618441a00d193e33c7b4b5e4e2d89860f0
Hattori-Arch <hattori.index.js@asagi.waseda.jp>
2026-04-28 16:23:10 +0900

    PWA化解除 - Cloudflare Workersとキャッシュの相性が悪い

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,48 +5,51 @@ import { VitePWA } from "vite-plugin-pwa"
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      includeAssets: ["icon/*.png"],
-      manifest: {
-        name: "Kadai Box",
-        short_name: "Kadai Box",
-        description: "Kadai Box",
-        theme_color: "#ffffff",
-        background_color: "#ffffff",
-        display: "standalone",
-        start_url: "/",
-        scope: "/",
-        icons: [
-          {
-            src: "icon/192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "icon/512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "icon/512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
-        cleanupOutdatedCaches: true,
-      },
-      devOptions: {
-        enabled: true,
-      },
-    }),
+    // VitePWA({
+    //   registerType: "autoUpdate",
+    //   injectRegister: "auto",
+    //   includeAssets: ["icon/*.png"],
+    //   manifest: {
+    //     name: "Kadai Box",
+    //     short_name: "Kadai Box",
+    //     description: "Kadai Box",
+    //     theme_color: "#ffffff",
+    //     background_color: "#ffffff",
+    //     display: "standalone",
+    //     start_url: "/",
+    //     scope: "/",
+    //     icons: [
+    //       {
+    //         src: "icon/192x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "icon/512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "icon/512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "maskable",
+    //       },
+    //     ],
+    //   },
+    //   workbox: {
+    //     globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+    //     cleanupOutdatedCaches: true,
+    //   },
+    //   devOptions: {
+    //     enabled: true,
+    //   },
+    // }),
   ],
   server: {
     host: true,
+  },
+  build: {
+    outDir: "../api/dist",
   },
 })
